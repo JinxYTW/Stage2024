7b24d6066dae879da75d26a82e5b71dbb12b3f2c
JinxYTW <tyohann974@gmail.com>
2024-07-17 12:17:18 +0200

    Ajout de logs

--- front/js/home.js
+++ front/js/home.js
@@ -6,19 +6,21 @@ async function run() {
     const baseUrl = "localhost:8080"; 
 
     const sseClientWaiting = new SSEClient(baseUrl);
-    console.log('SSEClient:', sseClientWaiting);
+    
 
     try {
         console.log('Tentative de connexion et d\'abonnement à l\'événement SSE...');
         await sseClientWaiting.connect();
         console.log('Connexion SSE réussie');
 
+        console.log('Abonnement à l\'événement \'newDemande\'...');
         // Abonnement à l'événement 'newDemande'
         await sseClientWaiting.subscribe('newDemande', (data) => {
             console.log('Nouvelle demande créée :', data);
             // Mettez à jour l'interface utilisateur avec les données de la nouvelle demande
             // Exemple : ajouter la demande à la liste des demandes récentes sur votre page
         });
+        console.log('Abonnement réussi à l\'événement \'newDemande\'');
 
         
         window.addEventListener('unload', () => {
