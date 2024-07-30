fbfbb2ee22174dd242d8905a21a30df7d80291b3
JinxYTW <tyohann974@gmail.com>
2024-07-30 09:48:02 +0200

    Enregistrement chemin dans bdd

--- back/src/controller/DevisController.java
+++ back/src/controller/DevisController.java
@@ -5,6 +5,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+import java.nio.file.Path;
+
 import com.google.gson.JsonObject;
 
 import webserver.WebServerContext;
@@ -18,39 +22,79 @@ public class DevisController {
     }
 
     public void uploadDevis(WebServerContext context) {
-        int demandeId = Integer.parseInt(context.getRequest().getQueryParams().get("demandeId"));
         try {
             // Récupérer le fichier envoyé dans la requête
             InputStream fileInputStream = context.getRequest().getInputStream();
-            String fileName = context.getRequest().getHeader("filename"); // Vous devez envoyer le nom du fichier dans l'en-tête de la requête
-            System.out.println("Nom du fichier : " + fileName);
-            File file = new File("back/src/pdf/Devis/" + fileName);
-            System.out.println("Chemin du fichier : " + file.getAbsolutePath());
+            String fileName = context.getRequest().getHeader("filename");
+            int demandeId = Integer.parseInt(context.getRequest().getQueryParams().get("demandeId"));
+            
+            // Définir le répertoire où les fichiers seront stockés
+            File directory = new File("back/src/pdf/Devis");
+            if (!directory.exists()) {
+                directory.mkdirs(); // Crée le répertoire s'il n'existe pas
+            }
+    
+            File file = new File(directory, fileName);
             FileOutputStream fileOutputStream = new FileOutputStream(file);
-            System.out.println("Début de la copie du fichier");
-
+    
             byte[] buffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = fileInputStream.read(buffer)) != -1) {
                 fileOutputStream.write(buffer, 0, bytesRead);
             }
-            System.out.println("Fin de la copie du fichier");
-
+    
             fileInputStream.close();
             fileOutputStream.close();
-
-            String filePath = file.getCanonicalPath();
-            System.out.println("Chemin du fichier : " + filePath);
-
-            DevisDao.saveDevisToDatabase(demandeId,filePath);
-            
+    
+            // Obtenez le chemin relatif du fichier
+            String relativePath = getRelativePath(file.getCanonicalPath(), directory.getCanonicalPath());
+            System.out.println("Relative path: " + relativePath);
+    
+            // Enregistrez le chemin relatif dans la base de données
+            DevisDao.saveDevisToDatabase(demandeId, relativePath);
+    
             context.getResponse().ok("Fichier téléchargé avec succès");
         } catch (IOException e) {
             e.printStackTrace();
             context.getResponse().serverError("Erreur lors du téléchargement du fichier");
         }
     }
-
+    
+    private String getRelativePath(String filePath, String baseDir) {
+        // Convertir les chemins en chaînes de caractères avec des barres obliques pour compatibilité multiplateforme
+        String absolutePath = new File(filePath).getAbsolutePath().replace("\\", "/");
+        String basePath = new File(baseDir).getAbsolutePath().replace("\\", "/");
+    
+        // Assurez-vous que basePath se termine par /
+        if (!basePath.endsWith("/")) {
+            basePath += "/";
+        }
+    
+        // Assurez-vous que basePath est un préfixe de absolutePath
+        if (absolutePath.startsWith(basePath)) {
+            // Extraire le chemin relatif en utilisant substring
+            String relativePath = absolutePath.substring(basePath.length());
+    
+            // Assurez-vous que le chemin relatif commence par /
+            if (!relativePath.startsWith("/")) {
+                relativePath = "/" + relativePath;
+            }
+    
+            // Construire le chemin relatif personnalisé
+            String customRelativePath = "/back/src/pdf/Devis" + relativePath;
+    
+            
+            System.out.println("Custom Relative path: " + customRelativePath);
+    
+            return customRelativePath;
+        } else {
+            // Le basePath n'est pas un préfixe du absolutePath, retournez une chaîne vide ou gérez l'erreur
+            System.err.println("Base path is not a prefix of the absolute path.");
+            return "";
+        }
+    }
+    
+    
     public void generatePdf(WebServerContext context) {
         WebServerResponse response = context.getResponse();
         
